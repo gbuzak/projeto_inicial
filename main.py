067382b14190d0d62300ae38ab31c90bb987924c
Gabriel <gbuzak@gmail.com>
2024-11-29 20:59:57 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Jornada de Dados")
+
+#Variaveis
+idade = 35
+nome = "Gabriel"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 10
+print(nova_idade)
+
+#Lista
+lista = [1,2,3,4]
+
+#Métodos
+lista_invertida = lista.reverse()
+
+
